0559b40c35f4fac9a4a8c41603514f49ce89f642
yangzilong <yangzilong@hungrypandagroup.com>
2022-06-14 18:25:01 +0800

    feat: 增加app-routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import("./pages/login/login.module").then(m => m.LoginModule),
+  },
+  {
+    path: 'search',
+    loadChildren: () => import("./pages/search/search.module").then(m => m.SearchModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
