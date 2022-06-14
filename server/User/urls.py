c9b7c3f3f05f3bf68590f4f650d1231513580d4c
yangzilong <yangzilong@hungrypandagroup.com>
2022-06-14 19:33:18 +0800

    feat: 前后端仓库分离

--- server/User/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from User.views import login, register
-urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register')
-]
