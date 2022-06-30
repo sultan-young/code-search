fcd8054bad72c12bade654bcced446f19dbced52
yangzilong <yangzilong@hungrypandagroup.com>
2022-06-30 11:40:53 +0800

    feat: asdf
    123
    
    BREAKING CHANGE :
    123

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
 module.exports = { 
+  extends: [
+    "@commitlint/config-conventional"
+  ],
   rules: {
     // @see: https://commitlint.js.org/#/reference-rules
     'type-enum': [
@@ -24,13 +27,13 @@ module.exports = {
     messages: {
       type: "选择你要提交的类型 :",
       scope: "选择一个提交范围（可选）:",
-      customScope: "请输入自定义的提交范围 :",
-      subject: "填写简短精炼的变更描述 :\n",
-      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-      footerPrefixsSelect: "选择关联issue前缀（可选）:",
-      customFooterPrefixs: "输入自定义issue前缀 :",
-      footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
+      // customScope: "请输入自定义的提交范围 :",
+      // subject: "填写简短精炼的变更描述 :\n",
+      // body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+      // breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      // footerPrefixsSelect: "选择关联issue前缀（可选）:",
+      // customFooterPrefixs: "输入自定义issue前缀 :",
+      // footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
       confirmCommit: "是否提交或修改commit ?"
     },
     types: [
